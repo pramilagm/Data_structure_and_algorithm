242541d1ab380f4c2f9dd320e96b6bd7f8dda99a
pramilagm <pmiris.pramila@gmail.com>
2020-05-18 12:29:44 -0700

    stack implementation using Linked List

---
+++ stack/stackImpleusingLL.py
@@ -0,0 +1,39 @@
+class StackNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.root = None
+
+    def isEmpty(self):
+        return True if self.root is None else False
+
+    def push(self, data):
+        newNode = StackNode(data)
+        newNode.next = self.root
+        self.root = newNode
+        print('%d pushed to stack' % data)
+
+    def pop(self):
+        if self.isEmpty():
+            return float("-inf")
+        deletedNode = self.root
+        self.root = self.root.next
+        popped = deletedNode.data
+        return popped
+
+    def peek(self):
+        if self.isEmpty():
+            return float("-inf")
+        return self.root.data
+
+
+stack = Stack()
+stack.push(10)
+stack.push(20)
+stack.push(30)
+popped = stack.pop()
+print(popped, ' poped from stack')

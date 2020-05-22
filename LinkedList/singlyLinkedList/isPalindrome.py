d2eeb531465968a04135d7e92e66db700d6fc690
pramilagm <pmiris.pramila@gmail.com>
2020-05-21 17:10:59 -0700

    to find the palindrome using singly linked list

--- LinkedList/singlyLinkedList/isPalindrome.py
+++ LinkedList/singlyLinkedList/isPalindrome.py
@@ -8,22 +8,36 @@ class SLL:
     def __init__(self):
         self.head = None
 
-    def push(self, data):
-        newNode = None(data)
-        if self.head is None:
-            self.head = newNode
-        currentNode = self.head
-        while currentNode.next is not None:
-            currentNode = currentNode.next
-        currentNode.next = newNode
+    # def push(self, data):
+    #     newNode = None(data)
+    #     if self.head is None:
+    #         self.head = newNode
+    #     currentNode = self.head
+    #     while currentNode.next is not None:
+    #         currentNode = currentNode.next
+    #     currentNode.next = newNode
 
     def isPalindrome(self):
-        left = self.head
-        right = self.head
-        while right.next is not None:
-            right = right.next
-        while right != left:
-            if left.data != right.data:
+        string = ''
+        currentNode = self.head
+        while currentNode.next is not None:
+            string += currentNode.data
+            currentNode = currentNode.next
+        return palindrome(string)
+
+    def palindrome(self, string):
+        i = 0
+        j = len(string)-1
+        while i < j:
+            if string[i] != string[j]:
                 return False
-            left = left.next
-            prevNode = right
+            i += 1
+            j -= 1
+        return True
+
+
+list = SLL()
+isPal = list.palindrome('racecar')
+isPal1 = list.palindrome('hello')
+print(isPal)
+print(isPal1)
